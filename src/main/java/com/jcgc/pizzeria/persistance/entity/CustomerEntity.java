c341cd99862c2b29b95673d097a8cd8e03f93f62
Juan Galvis <juan.galvis>
2024-03-05 10:16:06 -0500

    CustomerEntity table

---
+++ src/main/java/com/jcgc/pizzeria/persistance/entity/CustomerEntity.java
@@ -0,0 +1,36 @@
+package com.jcgc.pizzeria.persistance.entity;
+
+import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Column;
+import jakarta.persistence.GenerationType;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "customer")
+@Getter
+@Setter
+@NoArgsConstructor
+public class CustomerEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_customer", nullable = false)
+    private String idCustomer;
+
+    @Column(nullable = false, length = 60)
+    private String name;
+
+    @Column(nullable = false, length = 100)
+    private String address;
+
+    @Column(nullable = false, length = 50)
+    private String email;
+
+    @Column(name = "phone_number", nullable = false, length = 20)
+    private String phoneNumber;
+}
