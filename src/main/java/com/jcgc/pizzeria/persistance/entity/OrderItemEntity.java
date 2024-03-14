bcba431d1f64fa5738d3eb5d1382cae37840c074
Juan Galvis <juan.galvis>
2024-03-14 09:56:15 -0500

    Order Entity Controller and Service

--- src/main/java/com/jcgc/pizzeria/persistance/entity/OrderItemEntity.java
+++ src/main/java/com/jcgc/pizzeria/persistance/entity/OrderItemEntity.java
@@ -1,5 +1,6 @@
 package com.jcgc.pizzeria.persistance.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -31,6 +32,7 @@ public class OrderItemEntity {
     private Double price;
 
     @ManyToOne
+    @JsonIgnore
     @JoinColumn(name = "id_order", referencedColumnName = "id_order", insertable = false, updatable = false)
     private OrderEntity order;
 
