68b315b230c617def96e074020dc9581c25e0a6e
Juan Galvis <juan.galvis>
2024-03-31 11:30:15 -0500

    Using query methods - specific ones

--- src/main/java/com/jcgc/pizzeria/persistance/repository/PizzaRepository.java
+++ src/main/java/com/jcgc/pizzeria/persistance/repository/PizzaRepository.java
@@ -3,5 +3,9 @@ package com.jcgc.pizzeria.persistance.repository;
 import com.jcgc.pizzeria.persistance.entity.PizzaEntity;
 import org.springframework.data.repository.ListCrudRepository;
 
+import java.util.List;
+
 public interface PizzaRepository extends ListCrudRepository<PizzaEntity, Integer> {
+
+    List<PizzaEntity> findAllByAvailableTrueOrderByPrice();
 }
