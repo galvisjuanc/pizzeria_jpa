82776d901e8aef44a027f49e38260046261e4480
Juan Galvis <juan.galvis>
2024-04-02 23:08:42 -0500

    new service to get pizzas without ingredients

--- src/main/java/com/jcgc/pizzeria/service/PizzaService.java
+++ src/main/java/com/jcgc/pizzeria/service/PizzaService.java
@@ -33,6 +33,10 @@ public class PizzaService {
         return this.pizzaRepository.findAllByAvailableTrueAndDescriptionContainingIgnoreCase(ingredient);
     }
 
+    public List<PizzaEntity> getWithout(String ingredient) {
+        return this.pizzaRepository.findAllByAvailableTrueAndDescriptionNotContainingIgnoreCase(ingredient);
+    }
+
     public PizzaEntity get(int idPizza) {
         return this.pizzaRepository.findById(idPizza).orElse(null);
     }
