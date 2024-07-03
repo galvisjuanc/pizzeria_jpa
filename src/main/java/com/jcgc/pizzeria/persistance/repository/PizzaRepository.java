a912ce092ed2ec86aac4c2e45db060fb014be8aa
Juan Galvis <Galvis>
2024-07-03 15:55:05 -0500

    Use Modifying annotation in a Query Native

--- src/main/java/com/jcgc/pizzeria/persistance/repository/PizzaRepository.java
+++ src/main/java/com/jcgc/pizzeria/persistance/repository/PizzaRepository.java
@@ -1,7 +1,11 @@
 package com.jcgc.pizzeria.persistance.repository;
 
 import com.jcgc.pizzeria.persistance.entity.PizzaEntity;
+import com.jcgc.pizzeria.service.dto.UpdatePizzaPriceDto;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,4 +18,11 @@ public interface PizzaRepository extends ListCrudRepository<PizzaEntity, Integer
     List<PizzaEntity> findAllByAvailableTrueAndDescriptionNotContainingIgnoreCase(String description);
     List<PizzaEntity> findTop3ByAvailableTrueAndPriceLessThanEqualOrderByPriceAsc(double price);
     int countByVeganTrue();
+
+    @Query(value = """
+            UPDATE pizza SET price = :#{#newPizzaPrice.newPrice} WHERE id_pizza = :#{#newPizzaPrice.pizzaId}
+           """, nativeQuery = true)
+    //void updatePrice(@Param("idPizza") int idPizza, @Param("newPrice") double newPrice);
+    @Modifying
+    void updatePrice(@Param("newPizzaPrice") UpdatePizzaPriceDto newPizzaPrice);
 }
