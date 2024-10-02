af2f78c0fb39d80d586c7fab01eb71b98c0a385e
Juan Galvis <Galvis>
2024-10-02 16:13:07 -0500

    Use requestMatchers inside SecurityConfig

--- src/main/java/com/jcgc/pizzeria/web/config/SecurityConfig.java
+++ src/main/java/com/jcgc/pizzeria/web/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.jcgc.pizzeria.web.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -15,6 +16,8 @@ public class SecurityConfig {
 
         httpSecurity
                 .authorizeHttpRequests(customizeRequest -> customizeRequest
+                        .requestMatchers(HttpMethod.GET, "/api/**").permitAll()
+                        .requestMatchers(HttpMethod.PUT).denyAll()
                         .anyRequest()
                         .authenticated()
                 )
