a747df63ee182ade9481a58312d83285f0baa7d9
Juan Galvis <Galvis>
2024-10-10 14:17:37 -0500

    New Endpoint for CustomerController

--- src/main/java/com/jcgc/pizzeria/web/controller/CustomerController.java
+++ src/main/java/com/jcgc/pizzeria/web/controller/CustomerController.java
@@ -1,7 +1,10 @@
 package com.jcgc.pizzeria.web.controller;
 
 import com.jcgc.pizzeria.persistance.entity.CustomerEntity;
+import com.jcgc.pizzeria.persistance.entity.OrderEntity;
 import com.jcgc.pizzeria.service.CustomerService;
+import com.jcgc.pizzeria.service.OrderService;
+import org.hibernate.query.Order;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,18 +12,27 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/customers")
 public class CustomerController {
     private final CustomerService customerService;
+    private final OrderService orderService;
 
     @Autowired
-    public CustomerController(CustomerService customerService) {
+    public CustomerController(CustomerService customerService, OrderService orderService) {
         this.customerService = customerService;
+        this.orderService = orderService;
     }
 
     @GetMapping("/phone/{phone}")
     public ResponseEntity<CustomerEntity> getByPhone(@PathVariable String phone) {
         return ResponseEntity.ok(this.customerService.findByPhone(phone));
+    }
+
+    @GetMapping("/customer/{id}")
+    public ResponseEntity<List<OrderEntity>> getCustomerOrders(@PathVariable String id) {
+        return ResponseEntity.ok(this.orderService.getCustomerOrders(id));
     }
 }
