36fa359d765a90cc86e1818cc21cef077bc272ba
Juan Galvis <Galvis>
2024-10-18 15:44:33 -0500

    New Bean added in SecurityConfig.java

--- src/main/java/com/jcgc/pizzeria/web/config/SecurityConfig.java
+++ src/main/java/com/jcgc/pizzeria/web/config/SecurityConfig.java
@@ -3,7 +3,9 @@ package com.jcgc.pizzeria.web.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -39,5 +41,10 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 }
