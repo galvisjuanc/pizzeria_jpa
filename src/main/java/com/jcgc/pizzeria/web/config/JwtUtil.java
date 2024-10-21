8c6675ad1e55da8a8f75b60132263fa057edd71a
Juan Galvis <Galvis>
2024-10-21 10:44:41 -0500

    isValid Method added to test jwt

--- src/main/java/com/jcgc/pizzeria/web/config/JwtUtil.java
+++ src/main/java/com/jcgc/pizzeria/web/config/JwtUtil.java
@@ -2,6 +2,7 @@ package com.jcgc.pizzeria.web.config;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -21,4 +22,17 @@ public class JwtUtil {
                 .withExpiresAt(new Date(System.currentTimeMillis() + TimeUnit.DAYS.toMillis(15)))
                 .sign(ALGORITHM);
     }
+
+    public boolean isValid(String jwt) {
+        try {
+            JWT.require(ALGORITHM)
+                    .build()
+                    .verify(jwt);
+
+            return true;
+        } catch (JWTVerificationException e) {
+            return false;
+        }
+    }
+
 }
